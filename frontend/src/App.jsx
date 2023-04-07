9ca454bdd9bbb73a89c3fc7483bbf97b693e33ee
ar78studio <info@ar78.studio>
2023-04-07 16:08:19 +0200

    update css and menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import { Hero, WhatIsVip, FeelSafe, AVipPhone, Faq, ContactUs, Footer } from './components';
+import Menu from './components/nav/Menu';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
+			<div>
+				<Menu />
+			</div>
 			<div>
 				<Hero />
 			</div>
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
+import Menu from './nav/Menu';
 import Hero from './Hero';
 import WhatIsVip from './WhatIsVip';
 import FeelSafe from './FeelSafe';
@@ -6,4 +7,4 @@ import Faq from './Faq';
 import ContactUs from './ContactUs';
 import Footer from './Footer';
 
-export { Hero, WhatIsVip, FeelSafe, AVipPhone, Faq, ContactUs, Footer };
+export { Menu, Hero, WhatIsVip, FeelSafe, AVipPhone, Faq, ContactUs, Footer };
