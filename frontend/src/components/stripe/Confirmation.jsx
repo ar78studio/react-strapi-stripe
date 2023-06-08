7f8dcb6589bf2f175180b36c68a9b8d8c2257a13
ar78studio <info@ar78.studio>
2023-06-08 17:05:47 +0200

    Subscsription Confirmation styled

--- frontend/src/components/stripe/Confirmation.jsx
+++ frontend/src/components/stripe/Confirmation.jsx
@@ -25,19 +25,19 @@ const Confirmation = () => {
 				<div className='w-full flex-row bg-underNavBar p-3'></div>
 			</motion.div>
 			<section className='flex justify-center'>
-				<div className='flex flex-col justify-center items-center py-20 bg-purple-300 m-10 w-[700px] rounded-lg'>
-					<h1 className='text-4xl text-buttonColor'>Your Subscription is Confirmed </h1>
-					<span className='pt-10 text-xl text-buttonColor'> Your new Phone number is: </span>
-					<span className='pt-4 pb-10 text-2xl text-buttonColor'>{profileNumber ? profileNumber : 'Not available'}</span>
+				<div className='flex flex-col justify-center items-center p-10 bg-purple-300 m-10 lg:w-[700px] rounded-lg'>
+					<h1 className='text-2xl lg:text-4xl text-center text-buttonColor'>Your Subscription is Confirmed </h1>
+					<span className='pt-10 text-xl text-center text-buttonColor'> Your new Phone number is: </span>
+					<span className='bg-purple-600 p-4 rounded-lg mt-4 mb-10 text-2xl text-white'>{profileNumber ? profileNumber : 'Not available'}</span>
 
-					<h4>You can download an app for your Android Phone here:</h4>
-					<a className='text-md text-buttonColor' href='https://play.google.com/store/apps/details?id=net.riptec.conxhub'>
+					{/* <h4 className='text-center text-lg text-white font-semibold'>You can download an app for your Android Phone here:</h4> */}
+					<a className='text-md text-buttonColor text-lg font-semibold' href='https://play.google.com/store/apps/details?id=net.riptec.conxhub'>
 						{' '}
-						Android APP
+						Download Android APP
 					</a>
-					<h4>You can download an app for your iPhone here:</h4>
-					<a className='text-md text-buttonColor' href='https://apps.apple.com/es/app/conxhub/id1444346672'>
-						iOS iPhone APP
+					{/* <h4 className='text-center text-lg text-white font-semibold'>You can download an app for your iPhone here:</h4> */}
+					<a className='text-md text-buttonColor text-lg font-semibold' href='https://apps.apple.com/es/app/conxhub/id1444346672'>
+						Download iOS iPhone APP
 					</a>
 				</div>
 			</section>
