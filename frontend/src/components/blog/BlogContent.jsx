87702dbabbb9433340aaec16cd465f9e77886ca3
ar78studio <info@ar78.studio>
2023-06-08 17:25:55 +0200

    blog update

--- frontend/src/components/blog/BlogContent.jsx
+++ frontend/src/components/blog/BlogContent.jsx
@@ -50,7 +50,7 @@ const BlogContent = () => {
 		return <p>Error ...</p>;
 	}
 
-	console.log(data);
+	// console.log(data);
 
 	return (
 		<>
@@ -71,11 +71,11 @@ const BlogContent = () => {
 				<section className='w-full lg:max-w-[70%]'>
 					<div className='bg-purple-50 p-6 lg:pl-10 w-full flex flex-col justify-center items-center pt-20'>
 						<div className='flex flex-col justify-center items-center'>
-							<h4 className='text-3xl lg:text-5xl text-center pb-10'>{data.blog.data.attributes.title}</h4>
+							<h4 className='text-3xl lg:text-5xl text-center text-buttonColor pb-10'>{data.blog.data.attributes.title}</h4>
 							<img className='max-w-screen-md object-cover rounded-lg' src={`http://localhost:1337${data.blog.data.attributes.coverImage.data.attributes.formats}`} alt='Image' />
 
 							<div className='pt-10'>
-								<p className='text-base pb-2'>{data.blog.data.attributes.blogContent}</p>
+								<p className='text-base pb-2 text-buttonColor'>{data.blog.data.attributes.blogContent}</p>
 								<p className='text-md text-purple-500 py-6 '>
 									Categories:
 									{data.blog.data.attributes.categories.data.map((category) => (
