35634eb114dfbf16aec1829240b5a91c9c292fe0
ar78studio <info@ar78.studio>
2023-07-27 17:54:54 +0200

    cleanup

--- frontend/src/components/recoilState/state.js
+++
@@ -1,7 +0,0 @@
-// state.js
-import { atom } from 'recoil';
-
-export const discountCodeState = atom({
-	key: 'discountCodeState', // unique ID (with respect to other atoms/selectors)
-	default: '', // default value (aka initial value)
-});
